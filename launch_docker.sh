1bfbd1bdace26bb0e39cacc3c6779ca4daf87ace
Dustin Freeman <dustin.freeman@gmail.com>
2018-09-13 15:53:41 -0400

    Update Docker instructions with separate pre and post scripts. Tested on fresh Ubuntu install

--- launch_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo nvidia-docker build -t vid2vid:CUDA8-py35 .
-
-#run first time without /bin/bash on the end to do install from CMD in Dockerfile
-sudo nvidia-docker run --rm -ti --ipc=host -v $(pwd):/vid2vid --workdir=/vid2vid vid2vid:CUDA8-py35
