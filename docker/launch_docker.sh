910088f2d07614611f764f73c238a324fcdf5f1c
Dustin Freeman <dustin.freeman@gmail.com>
2018-09-13 17:30:17 -0400

    Launch Docker script increase shm size. Noticed this was required while running train_256_g1.sh

--- docker/launch_docker.sh
+++ docker/launch_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo nvidia-docker build -t vid2vid:CUDA8-py35 .
 
-sudo nvidia-docker run --rm -ti --ipc=host -v $(pwd):/vid2vid --workdir=/vid2vid vid2vid:CUDA8-py35 /bin/bash
+sudo nvidia-docker run --rm -ti --ipc=host --shm-size 8G -v $(pwd):/vid2vid --workdir=/vid2vid vid2vid:CUDA8-py35 /bin/bash
