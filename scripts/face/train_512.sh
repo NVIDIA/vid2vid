0adb7b0fcc464b29919fb7dc8702b62ece8ddc89
Ting-Chun Wang <tingchunw@nvidia.com>
2018-09-19 21:16:03 +0000

    add docker files

--- scripts/face/train_512.sh
+++ scripts/face/train_512.sh
@@ -2,4 +2,4 @@ python train.py --name edge2face_512 \
 --dataroot datasets/face/ --dataset_mode face \
 --input_nc 15 --loadSize 512 --num_D 3 \
 --gpu_ids 0,1,2,3,4,5,6,7 --n_gpus_gen 6 \
---n_frames_total 12
+--niter 20 --niter_decay 20 --n_frames_total 12
